2f58be93b592524e8d3ede4a11dfe30771210464
Steven Kay <stevendkay@gmail.com>
2018-11-20 17:02:36 +0000

    added remaining MVP it()s

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -5,8 +5,23 @@ describe('Park',function () {
 
   it("should have a name");
 
-  it("should have A ticket price");
-  
-  it("should have A collection of dinosaurs");
+  it("should have a ticket price");
+
+  it("should have a collection of dinosaurs");
+
+  it("should be possible to Add a dinosaur to its collection of dinosaurs");
+
+  it("should be possible to Remove a dinosaur from its collection of dinosaurs");
+
+  it("should be possible to Find the dinosaur that attracts the most visitors");
+
+  it("should be possible to Find all dinosaurs of a particular species");
+
+  it("should be possible to Calculate the total number of visitors per day");
+
+  it("should be possible to Calculate the total number of visitors per year");
+
+  it("should be possible to Calculate the total revenue from ticket sales for one year");
+
 
 });
